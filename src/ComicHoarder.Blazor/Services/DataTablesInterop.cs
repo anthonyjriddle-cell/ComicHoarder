ff732f60ccb6078894e88e6b80397a2a7550214f
tony riddle <anthonyjriddle@gmail.com>
2026-06-20 01:29:10 -0400

    Use QuickGrid for Publisher/Volume/Issue

---
+++ src/ComicHoarder.Blazor/Services/DataTablesInterop.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.JSInterop;
+
+namespace ComicHoarder.Blazor.Services
+{
+    public class DataTablesInterop
+    {
+        private readonly IJSRuntime _jsRuntime;
+
+        public DataTablesInterop(IJSRuntime jsRuntime)
+        {
+            _jsRuntime = jsRuntime;
+        }
+
+        public async Task InitDataTableAsync(string tableId)
+        {
+            await _jsRuntime.InvokeVoidAsync("dataTablesInterop.initDataTable", tableId);
+        }
+
+        public async Task DestroyDataTableAsync(string tableId)
+        {
+            await _jsRuntime.InvokeVoidAsync("dataTablesInterop.destroyDataTable", tableId);
+        }
+    }
+}
